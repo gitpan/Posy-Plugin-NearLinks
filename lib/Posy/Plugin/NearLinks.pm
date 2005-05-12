4f268ec9425bfc26850e0488896b50734dcf2ece
Kathryn Andersen <rubykat@cpan.org>
2005-05-12 11:49:35 +0000

    Import of RUBYKAT/Posy-Plugin-NearLinks-0.42 from CPAN.
    gitpan-cpan-distribution: Posy-Plugin-NearLinks
    gitpan-cpan-version:      0.42
    gitpan-cpan-path:         RUBYKAT/Posy-Plugin-NearLinks-0.42.tar.gz
    gitpan-cpan-author:       RUBYKAT
    gitpan-cpan-maturity:     released

--- lib/Posy/Plugin/NearLinks.pm
+++ lib/Posy/Plugin/NearLinks.pm
@@ -7,11 +7,11 @@ Posy::Plugin::NearLinks - Posy plugin to give links of articles in the current c
 
 =head1 VERSION
 
-This describes version B<0.41> of Posy::Plugin::NearLinks.
+This describes version B<0.42> of Posy::Plugin::NearLinks.
 
 =cut
 
-our $VERSION = '0.41';
+our $VERSION = '0.42';
 
 =head1 SYNOPSIS
 
